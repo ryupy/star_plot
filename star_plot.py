f481c94c7c050452fc776b3b504fed97ec56408e
ryupy <filaaas216@gmail.com>
2021-07-29 23:58:29 +0900

    update

--- star_plot.py
+++ star_plot.py
@@ -5,27 +5,44 @@ import matplotlib.pyplot as plt
 import math
 import sys
 
-# 標準入力は
-# 赤経,赤緯,等級
+#入力は
+# 0 HIP番号
+# 1 赤経時
+# 2 赤経分
+# 3 赤経秒
+# 4 赤緯符号
+# 5 赤緯度
+# 6 赤緯分
+# 7 赤緯秒
+# 8 等級
 
-N = 3009 #5.5等級までの星の数
 
+#星の数
+N = 3215
+plot_data = []
 
 with open('star_plot.txt', 'r', encoding='utf-8') as star:
     star_data = star.readlines()
     for i in range(N):
         star_data[i] = star_data[i].split(',')
-        star_data[i][2] = int(round(float(star_data[i][2]), 0))
+        a = float(float(star_data[i][1])*15 + float(star_data[i][2])*0.25 + float(star_data[i][3])*0.0042)
+        b = float(float(star_data[i][5]) + float(star_data[i][6])/60 + float(star_data[i][7])/3600)
+        c = int(round(float(star_data[i][8]), 0))
+        # d = str(star_data[i][16])
+        if star_data[i][4] == "0":
+            b = -b
+        if c <= 5:
+            plot_data.append([a,b,c,i])
 star.close()
 
 x = []
 y = []
 blt = []
 
-for i in range(N):
-    x_ = round(float(star_data[i][0]), 1)
-    y_ = round(float(star_data[i][1]), 1)
-    blt_ = star_data[i][2]
+for i in range(len(plot_data)):
+    x_ = round(float(plot_data[i][0]), 1)
+    y_ = round(float(plot_data[i][1]), 1)
+    blt_ = plot_data[i][2]
 
     x.append(x_)
     y.append(y_)
@@ -33,12 +50,13 @@ for i in range(N):
 
 plt.figure(figsize=(36, 9), dpi=300)
 plt.xlim(0, 360)
-plt.ylim(0, 90)
-plt.axis("off")
+plt.ylim(-90, 90)
+plt.axis("on")
 
-for i in range(N):
-    s_ = (6 - blt[i])**2
-    plt.scatter(x[i], y[i], s = s_)
 
+for i in range(len(plot_data)):
+    s_ = (6 - blt[i])**3
+    print(s_)
+    plt.scatter(x[i], y[i], s=s_, c=s_, cmap='spring', vmin=-50, vmax=343)
 plt.savefig("star_fig.png")
 # plt.show()
